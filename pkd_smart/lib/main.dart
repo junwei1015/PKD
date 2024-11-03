8bceca17a8df5e75537630247c3bb7eec03de1c2
junwei1015 <junweitee7@gmail.com>
2024-11-03 22:15:06 +0800

    add-Attendance

--- pkd_smart/lib/main.dart
+++ pkd_smart/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pkd_smart/pages/scan_code_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,25 +14,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Gladiator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        "/scan": (context) => const ScanCodePage(),
+      },
+      home: const MyHomePage(title: 'Flutter'),
     );
   }
 }
@@ -85,6 +74,16 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [
+          IconButton(
+          onPressed: () {
+            Navigator.popAndPushNamed(context, "/scan");
+          }, 
+          icon: const Icon(
+            Icons.qr_code_scanner,
+          ),
+          ),
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -112,6 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            
           ],
         ),
       ),
@@ -119,7 +119,9 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      
+       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ pkd_smart/lib/pages/scan_code_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ScanCodePage extends StatefulWidget {
+  const ScanCodePage({super.key});
+
+  @override
+  State<ScanCodePage> createState() => _ScanCodePageState();
+}
+
+class _ScanCodePageState extends State<ScanCodePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scan QR Code'),
+      ),
+    );
+  }
+}
